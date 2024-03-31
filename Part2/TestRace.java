154b5d3fd7b25b9f6a6a61afee0d4fb2d15c8a22
Kian Chong <kianchristianchong@gmail.com>
2024-03-31 21:22:02 +0100

    Test updates

--- Part2/TestRace.java
+++ Part2/TestRace.java
@@ -1,15 +1,15 @@
 public class TestRace {
     public static void main(String[] args) {
-        Race race = new Race(20);
-        race.setLanes(8);
+        Race race = new Race(20, 8);
+        //race.setLanes(5);
         Horse horse1 = new Horse('A', "ALPHA", 0.8);
         Horse horse2 = new Horse('B', "BETA", 0.4);
         Horse horse3 = new Horse('C', "CHARLIE", 0.6);
         Horse horse4 = new Horse('D', "DELTA", 0.75);
         Horse horse5 = new Horse('E', "ECHO", 0.7);
-        race.addHorse(horse1, 1);
+        race.addHorse(horse1, 3);
         race.addHorse(horse2, 2);
-        race.addHorse(horse3, 3);
+        race.addHorse(horse3, 7);
         race.addHorse(horse4, 4);
         race.addHorse(horse5, 5);
         race.startRace();
